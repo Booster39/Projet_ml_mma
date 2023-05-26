bab48012a7d50d1aff63f6da70e72c0e680c5d56
ilyesned <ilyesned@hotmail.com>
2023-05-26 18:51:30 +0200

    correction d'un bug f64 et vecteur

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 mod perceptron;
 use perceptron::{MyMLP};
 
-
 fn main() {
     let mut mlp = MyMLP::new(vec![2, 0, 1]);
     mlp._propagate(&[0.0, 0.0], false);
     println!("{:?}", mlp.X);
-
-
-
 
     let mut test_1_all_samples_inputs: Vec<Vec<f64>> = vec![
         vec![0.0, 0.0],
@@ -16,13 +12,11 @@ fn main() {
         vec![1.0, 0.0]
     ];
 
-
     let test_1_all_samples_expected_outputs: Vec<Vec<f64>> = vec![
         vec![1.0],
         vec![-1.0],
         vec![-1.0]
     ];
-
 
     // Afficher les résultats avant l'entraînement
     println!("Résultats avant l'entraînement :");
@@ -34,7 +28,7 @@ fn main() {
     //train
     let layer_sizes = vec![2, 0, 1];
     let mut train = MyMLP::new(layer_sizes);
-    let all_samples_inputs: Vec<Vec<f64>> = test_1_all_samples_inputs;
+    let all_samples_inputs: Vec<Vec<f64>> = test_1_all_samples_inputs.clone();
     let all_samples_expected_outputs: Vec<Vec<f64>> = test_1_all_samples_expected_outputs;
     let is_classification = true;
     let iteration_count = 100000;
@@ -51,16 +45,7 @@ fn main() {
     // Afficher les résultats après l'entraînement
     println!("Résultats après l'entraînement :");
     for inputs in &test_1_all_samples_inputs {
-        let predictions = mlp.predict(inputs, true);
+        let predictions = mlp.predict(inputs, false);
         println!("Entrées : {:?} | Prédictions : {:?}", inputs, predictions);
     }
-
-
-
-
-
-
-
-
-
 }
