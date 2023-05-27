1b99c7dbefc2e75972f2656bd668b0e163e558b5
Booster39 <abdiame10@yahoo.fr>
2023-05-27 22:18:28 +0200

    Suppressing deprecated code

--- src/process.rs
+++ src/process.rs
@@ -68,67 +68,8 @@ pub fn put_image_in_array() {
     }
 }
 
-
-/*fn main() {
-    let image_path = "C:/Users/abdoulaye.doucoure/Desktop/Projet_ml_mma/dataset/conor_mcgregor/1.jpg";
-    load_and_preprocess_image(image_path);
-    //let preprocessed_image = load_and_preprocess_image(image_path);
-    //put_images_in_vector();
-}*/
-
-
-
-
-/*fn main() {
-    let image_path = "C:/Users/abdoulaye.doucoure/Desktop/Projet_ml_mma/dataset/conor_mcgregor/1.jpg";
-    let preprocessed_image = load_and_preprocess_image(image_path);
-
-    // Use the preprocessed image for machine learning
-    // ...
-}
-
-main()
-*/
-
-/*fn put_image_in_array() {
-    // Créer un vecteur de deux lignes où chaque élément est un vecteur
-    let mut matrix: Vec<f32> = Vec::new();
-    for conor in 0..30 {
-        normalized_img_data.push(conor);
-    }
-    for tyson in 0..30 {
-        normalized_img_data.push(tyson);
-    }
-
-    let mut matrix: Vec<Vec<T>> = Vec::new(); 
-    for i in 1..31 {
-        let mut link: String = "./dataset/conor_mcgregor/";
-        link.push(i.to_string());
-        matrix.push(load_and_preprocess_image(link))
-    }
-    matrix.transpose();
-    let mut vector: Vec<Vec<T>> = Vec::new(); 
-    for i in 1..29 {
-        let mut link: String = "./dataset/mike_tyson/";
-        link.push(i.to_string());
-        vector.push(load_and_preprocess_image(link))
-    }
-    vector.transpose();
-    matrix.push(vector[0]);
-
-    //PROBLEME RESOLU !!!!!!!
-    //But : On ne veut pas seulement 3 éléments par vecteurs mais on en veut 30. Pour cela
-    //le mieux est de faire une boucle for qui ajoute chaque élément un par un. (avec push)
-    //-> Problème : après avoir rempli le 1er vec -> les éléments s'affichent en colonnes et pas en lignes
-    //Donc après le 1 er vec, on utilise transpose pour que le 1er vec corresponde à la 1re ligne
-    //On effectue la même opération pour le 2ème vec
-
-    // Afficher le contenu du vecteur
-    println!("{:?}", matrix);
-}
 //1 et -1 sur photo -> tous les labels (Y_train)
 //X_train 
 
 //0 1 2 3 4 5
 //6 7 8 9 0 0
-*/
